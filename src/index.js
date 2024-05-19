39fc4d4fe0c5d18026dd3b8ca1e5111c64c8c56b
Sunimal Malkaklulage <sskmal@gmail.com>
2024-05-19 17:55:59 +0900

    Update index book list
    Book list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-const Greeting = () => {
-    return <h2>My First Component</h2>;
-  };
-  export default Greeting;
+import './index.css';
+
+
+
+const BookList = () => {
+
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+
+  );
+
+}
+
+const Book = () => {
+
+  return (
+    <article className='book'>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+
+  );
+}
+
+
+const Image = () => (
+  <img src='https://images-na.ssl-images-amazon.com/images/I/714vGrppcgL._AC_UL600_SR600,400_.jpg'
+  alt='book'></img>
+);
+const Title = () => {
+  return <h2>Good Energy</h2>;
+};
+const Author = () => <h4>by Casey Means MD (Author), Calley Means</h4>;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<BookList />);
+
